c94329d2a76a0c9db1b5ba39fcaea67e2177c9ad
Beg24 <hulrickmombo@gmail.com>
2024-05-06 18:43:39 +0100

    exo2 main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,8 @@
-# Écrivez votre code ici !
+liste_fruits =["pomme", "banane", "orange"]
+print (liste_fruits)
+liste_fruits.append ("kiwi")
+print (liste_fruits)
+liste_fruit.remove ('orange')
+liste_fruits [1]= 'ananas'
+liste_fruits = ["banane", "orange", "pomme"]
+print (liste_fruits)
